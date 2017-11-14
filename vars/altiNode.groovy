f966a1bb4044b0f40ad57ff0f620b05e6c82168e
Noah Kantrowitz <noah@coderanger.net>
2017-11-14 19:20:09 -0200

    Pull the artifactoryToken from the k8s secret rather than Jenkins.

--- vars/altiNode.groovy
+++ vars/altiNode.groovy
@@ -1,11 +1,12 @@
 def call(Closure body) {
-    withCredentials([string(credentialsId: 'artifactory-jenkins-dev', variable: 'ARTIFACTORY_API_KEY')]) {
-        withEnv(['CI=true', "BERKSHELF_PATH=${env.WORKSPACE}/.berkshelf", 'ALTISCALE_BERKS_ARTIFACTORY=true']) {
-            node('cookbook') {
-                container('alti-pipeline') {
-                    body()
-                }
-            }
-        }
+  def secretsRoot = System.getenv('JENKINS_SECRETS') ?: '/var/jenkins_secrets'
+  def artifactoryToken = new File("$secretsRoot/artifactory-token").text.trim()
+
+  withEnv(['CI=true', "BERKSHELF_PATH=${env.WORKSPACE}/.berkshelf", 'ALTISCALE_BERKS_ARTIFACTORY=true', "ARTIFACTORY_API_KEY=$artifactoryToken"]) {
+    node('cookbook') {
+      container('alti-pipeline') {
+        body()
+      }
     }
+  }
 }
